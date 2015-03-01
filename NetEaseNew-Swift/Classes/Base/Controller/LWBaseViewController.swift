9dc77bd21d9542fe41c8df585bf641d89d5b1523
wangshiyu13 <wangshiyu13@163.com>
2015-03-01 21:21:35 +0800

    update framework

--- NetEaseNew-Swift/Classes/Base/Controller/LWBaseViewController.swift
+++ NetEaseNew-Swift/Classes/Base/Controller/LWBaseViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  LWBaseViewController.swift
 //  NetEaseNew-Swift
 //
 //  Created by wangshiyu13 on 15/3/1.
@@ -8,18 +8,15 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class LWBaseViewController: UIViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        self.title = "zhuye"
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
-
---
+++ NetEaseNew-Swift/Classes/Home/Controller/LWHomeViewController.swift
@@ -0,0 +1,34 @@
+//
+//  LWHomeViewController.swift
+//  NetEaseNew-Swift
+//
+//  Created by wangshiyu13 on 15/3/1.
+//  Copyright (c) 2015年 LLWW. All rights reserved.
+//
+
+import UIKit
+
+class LWHomeViewController: LWBaseViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
